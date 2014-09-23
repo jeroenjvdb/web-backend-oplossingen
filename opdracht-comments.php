d7e3242cae92dd9a8ea0f5734f9d8b8e8ef93974
jeroenjvdb <jeroen.vandenbroeck@student.kdg.be>
2014-09-23 11:50:05 +0200

    eerste-oefening

---
+++ opdracht-comments.php
@@ -0,0 +1,16 @@
+<?php 
+	$naam = "Jeroen Van den Broeck";
+	#Jeroen Van den Broeck
+	//jeroen.vandenbroeck@student.kdg.be
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?= $naam ?>
+</body>
+</html>
