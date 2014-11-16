104e9a37d217592507573820cbc4163ce2cb77fb
jeroenjvdb <jeroen_vdb1@hotmail.com>
2014-11-16 12:34:30 +0100

    oefening include require

---
+++ include-require/index.php
@@ -0,0 +1,28 @@
+<?php
+	$artikels = array
+	(
+		array
+		(
+			"title" =>"IS claimt onthoofding Peter Kassig",
+			"text" => "De radicale islamitische groep Islamitische staat (IS) heeft een video op de jihadistische websites geplaatst, waarin ze de onthoofding...",
+			"tags" => array("buitenland", "IS")
+		), array
+		(
+			"title" => "vrouw belandt na ongeval onder eigen wagen",
+			"text" => "In Foy-Notre-Dame (nabij Dinant) is een vrouw zwaargewond geraakt bij een bizar verkeersongeval. De hulpdiensten vonden de vrouw onder haar eigen ...",
+			"tags" => array("binnenland", "verkeersongeval")
+		), array
+		(
+			"title" => "kerstman overvalt postkantoor",
+			"text" => "In Australië is de politie op zoek naar 'Bad Santa', ofwel een stoute kerstman. In Melbourne overviel gisteren een gangster verkleed als ...",
+			"tags" =>array("bizar")
+		)
+	);
+
+
+	include 'header.html';
+	include 'body.html';
+	include 'footer.html';
+
+?>
+
