8bb3c59f92c9631bfa05d062ec285db4abcb0b1f
jeroenjvdb <jeroen_vdb1@hotmail.com>
2014-12-15 13:50:53 +0100

    basis oefening mod_rewrite

---
+++ mod_rewrite-basis/deel2/original.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>original</title>
+</head>
+<body>
+	<h1>Het originele bestand</h1>
+	<?php (isset($_GET['student'])? var_dump($_GET['student']) : '') ?>
+</body>
+</html>
